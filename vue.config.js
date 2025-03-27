0c5e61d20ef3b3dcbb4d9c6ddcb76aac97701044
GunnerSupanut <gunsupm@gmail.com>
2025-03-27 21:51:11 +0700

    d

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/Kanbanboard/'  // ใช้ชื่อ repo ของคุณแทน
-    : '/'
+module.exports = {  
+
+  publicPath: '/Kanbanboard/'  
 }
